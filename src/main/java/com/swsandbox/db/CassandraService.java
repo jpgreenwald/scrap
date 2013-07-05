d09fbf9fd205ce49eaa31630aa73c1b35f34df94
Jesse Greenwald <jesse@greenwald>
2013-07-05 16:19:22 -0500

    added cassandra example

---
+++ src/main/java/com/swsandbox/db/CassandraService.java
@@ -0,0 +1,10 @@
+package com.swsandbox.db;
+
+/**
+ * User: jgreenwald
+ * Date: 7/5/13
+ * Time: 9:55 AM
+ */
+public class CassandraService
+{
+}
---
+++ src/main/java/com/swsandbox/db/Kv.java
@@ -0,0 +1,10 @@
+package com.swsandbox.db;
+
+/**
+ * User: jgreenwald
+ * Date: 7/5/13
+ * Time: 12:13 PM
+ */
+public class Kv
+{
+}
